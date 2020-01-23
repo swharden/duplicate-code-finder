2070819130cbe37f52773431852140739c4b1f2d
Scott W Harden <swharden@gmail.com>
2020-01-23 13:15:15 -0500

    Update duplicateCodeFinder.py

--- src/duplicateCodeFinder.py
+++ src/duplicateCodeFinder.py
@@ -36,9 +36,10 @@ def LoadSourceCode(sourceFiles):
 
 
 IGNOREDLINES = [
-    "{", "}", "}else{", "else", "else{", "break;",
+    "{", "}", "break;", "try {", "};",
+    "}else{", "else", "else{", "} else", 
     "return;", "return true;", "return false;",
-    "**/", "/**", "*/", "/*", ""
+    "**/", "/**", "*/", "/*", "", "continue;"
 ]
 
 
@@ -51,21 +52,41 @@ def GetLineCounts(sourceCodes):
         lines = sourceCode.split("\n")
         for lineNumber, line in enumerate(lines):
             line = line.strip()
+            line = line.replace("\t", " ")
+
+            # add spacing to special characters
+            specialChars = list("{}'\"();=")
+            for char in specialChars:
+                line = line.replace(char, " " + char + " ")
+
+            # ensure all padding is a single space
+            while "  " in line:
+                line = line.replace("  ", " ")
+
+            # add padding back to some characters
+            line = line.replace(" ;", ";")
+            line = line.replace(" (", "(")
+            line = line.replace(") ", ")")
+            line = line.replace("( ", "(")
+            line = line.replace(" )", ")")
+            line = line.strip()
+
             if not line in seenLines:
                 seenLines[line] = 0
             seenLines[line] += 1
     lineCounts = sorted(seenLines.items(), key=lambda x: x[1])
     return lineCounts
 
-def DisplayLineCounts(lineCounts):
-    assert isinstance(lineCounts, dict)
-    print(f"Count\tLine")
-    for line, count in sortedLines:
+def DisplayLineCounts(lineCounts, minimumRepeats = 10):
+    assert isinstance(lineCounts, list)
+    print(f"Count\tCode")
+    print("-----\t" + "-"*50)
+    for line, count in lineCounts[::-1]:
         if line.startswith("#") or line.startswith("//"):
             continue
         if line in IGNOREDLINES or line.replace(" ", "") in IGNOREDLINES:
             continue
-        if count < 5:
+        if count < minimumRepeats:
             continue
         print(f"{count}\t{line}")
 
@@ -85,5 +106,5 @@ if __name__ == "__main__":
 
     sourceFiles = GetSourceFiles(folderPath)
     sourceCodes = LoadSourceCode(sourceFiles)
-    lineCounts = CountLines(sourceCodes)
+    lineCounts = GetLineCounts(sourceCodes)
     DisplayLineCounts(lineCounts)
